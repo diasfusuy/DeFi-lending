6862117790ce5d6f40c43d200eb12875b616f816
diasfusuy <diasfusuy4@gmail.com>
2025-07-03 18:51:37 -0500

    Added MockETH deployment

--- ignition/modules/MockModule.js
+++ ignition/modules/MockModule.js
@@ -4,8 +4,9 @@ const MockModule = buildModule("MockModule", (m) => {
     const deployer = m.getAccount(0); // Deploy as initialOwner
 
     const mockUSDC = m.contract("MockUSDC", [deployer]);
+    const mockETH = m.contract("MockETH", [deployer]);
 
-    return { mockUSDC };
+    return { mockUSDC, mockETH };
 });
 
 module.exports = MockModule;
