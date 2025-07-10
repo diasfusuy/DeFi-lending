393dd5b8905a3aa15937752d589b8c3df9ef00f0
diasfusuy <diasfusuy4@gmail.com>
2025-07-10 02:52:35 -0500

    added polygn amoy to the hardhat config and downloaded dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    polygon_amoy: {
+      url: "https://rpc-amoy.polygon.technology",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
