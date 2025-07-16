d17fd962b4e70f9c04e0c35a28c6c3a13b8d8799
diasfusuy <diasfusuy4@gmail.com>
2025-07-15 21:57:57 -0500

    Installed eterscan to and added to hardhat config to verify contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  etherscan: {
+  apiKey: {
+    polygonAmoy: "BSDXSS6CJ4XVG63X3W1KXV6YAXS1Q6UNBA"
+  },
+},
   networks: {
     polygon_amoy: {
       url: "https://rpc-amoy.polygon.technology",
